2dd6fded3e15a48b8ab69072a0ca11514cb95a30
NumdyPixe1 <numdao123@gmail.com>
2025-04-25 21:07:21 +0700

    finish quiz-3

--- quiz-3/index.php
+++ quiz-3/index.php
@@ -5,11 +5,6 @@
  $arr2 = array(array("Code"=> 103,"City" => "Singapore"),
  array("Code"=> 102,"City" => "Tokyo"),
  array("Code"=> 101,"City" => "Bangkok"));
-
-
-//  $col1= array_column($arr1,'Code','Name');
-//  print_r($col1);
- 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +14,7 @@
     <title>quiz-3</title>
 </head>
 <body>
-    <b>Array 1</b>
+    <b>Array1</b>
     <table border="1">
     <thead>
             <tr>
@@ -30,82 +25,57 @@
         <tbody>
             <?php
             for($i = 0; $i < count($arr1); $i++){
-
             echo '<tr>'.
             '<td>'.$arr1[$i]["Code"].'</td>'.
             '<td>'.$arr1[$i]["Name"].'</td>';
             '</tr>';   
         }
             ?>
-        <!-- <tr>
-            <td><?php echo $arr1[0]["Code"];?></td>
-            <td><?php echo $arr1[0]["Name"];?></td>
-        </tr>
-        <tr>
-            <td><?php echo $arr1[1]["Code"];?></td>
-            <td><?php echo $arr1[1]["Name"];?></td>
-        </tr>
-        <tr>
-            <td><?php echo $arr1[2]["Code"];?></td>
-            <td><?php echo $arr1[2]["Name"];?></td>
-        </tr> -->
     </tbody>
 </table>
 
 <br>
-<b>Array 2</b>
+<b>Array2</b>
     <table border="1">
      <thead>
             <tr>
                 <th>Code</th>
-            <th>Name</th> 
+            <th>City</th> 
             </tr>
         </thead>
         <tbody>
-        <tr>
-            <td><?php echo $arr2[0]["Code"];?></td>
-            <td><?php echo $arr2[0]["City"];?></td>
-        </tr>
-        <tr>
-            <td><?php echo $arr2[1]["Code"];?></td>
-            <td><?php echo $arr2[1]["City"];?></td>
-        </tr>
-        <tr>
-            <td><?php echo $arr2[2]["Code"];?></td>
-            <td><?php echo $arr2[2]["City"];?></td>
-        </tr>
+        <?php
+            for($i = 0; $i < count($arr2); $i++){
+            echo '<tr>'.
+            '<td>'.$arr2[$i]["Code"].'</td>'.
+            '<td>'.$arr2[$i]["City"].'</td>';
+            '</tr>';   
+        }
+            ?>
     </tbody>
 </table>
 <br>
 <hr>
+<br>
 <b>Output</b>
     <table border="1">
     <thead>
             <tr>
                 <th>Code</th>
                 <th>Name</th> 
-                <th>Name</th>
+                <th>City</th>
             </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>101</td>
-            <td>AAA</td>
-            <td>CCC</td>
-
-        </tr>
-        <tr>
-            <td>102</td>
-            <td>BBB</td>
-            <td>CCC</td>
-
-        </tr>
-        <tr>
-            <td>103</td>
-            <td>CCC</td>
-            <td>CCC</td>
-
-        </tr>
+        <?php
+            for($i = 0; $i < count($arr1); $i++){
+            echo '<tr>'.
+             '<td>'.$arr1[$i]["Code"].'</td>'.
+            '<td>'.$arr1[$i]["Name"].'</td>'.
+            '<td>'.$arr2[$i]["City"].'</td>';
+            '</tr>';   
+        }
+            ?>
     </tbody>
 </table>
 </body>
