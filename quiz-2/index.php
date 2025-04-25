09cd30401a07ac48ab4ef77b1cd7350d06b07696
NumdyPixe1 <numdao123@gmail.com>
2025-04-25 17:29:19 +0700

    finish quiz-2

--- quiz-2/index.php
+++ quiz-2/index.php
@@ -1,19 +1,47 @@
 <?php
-    $result = null;
-  $value = $_POST['v1'];
+//   $result = isset($num) ? $num : '';  
+$v100 = $_POST['v100'];
+$v7 = $_POST['v7'];
+$v107 = $_POST['v107'];
+$v3 = $_POST['v3'];
+$v104 = $_POST['v104'];
+
   if(isset($_POST['cal'])){
-    
-       
-        $result = calculate($value);
-       // echo"<h1>$result</h1>";
+    if(empty($v100) && empty($v7)&& empty($v107)&& empty($v3) &&empty($v104)){
+     $v100= 0;$v7=0; $v107=0; $v3=0; $v104 = 0;
+    }else{
+        $percents = array( 7/100 , 107/100 , 3/100 , 104/100 ) ;
+        // $values = array($v100, $v7, $v107, $v3, $v104);
+  
+    if($v100){
+        $v7 = (float)$v100* $percents[0];//7
+        $v107 = (float)$v100* $percents[1];//107
+       $v3 = (float)$v100* $percents[2];//3
+       $v104 = (float)$v100* $percents[3];//104
+    } else if($v7){         
+        $v100 = (float)$v7 / $percents[0];// n / 7
+        $v107 = (float)$v100 * $percents[1];//107
+        $v3 = (float)$v100 * $percents[2];//3
+        $v104 = (float)$v100 * $percents[3];//104
+    } else if($v107){
+        $v100 = (float)$v107 / $percents[1];// n / 107
+        $v7 = (float)$v100* $percents[0];//7
+        $v3 = (float)$v100 * $percents[2];//3
+        $v104 = (float)$v100* $percents[3];//104
+    }else if($v3){
+        $v100 = (float)$v3 / $percents[2];// n / 3
+        $v7 = (float)$v100* $percents[0];//7
+        $v107 = (float)$v100 * $percents[1];//107
+        $v104 = (float)$v100* $percents[3];//104
+    }else if($v104){
+        $v100 = (float)$v104 / $percents[3];// n / 104
+        $v7 = (float)$v100* $percents[0];//7
+        $v107 = (float)$v100 * $percents[1];//107
+        $v3 = (float)$v100* $percents[2];//3
     }
-    function calculate($value){
-        $percents = array(100, 7, 107, 3, 104 );
-        foreach ($percents as $percent) {
-           
-        }
-     return  $value * 100;
+
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,16 +63,17 @@
         <th>104</th>
     </tr>
     <tr>
+    <!-- value=<?php echo"$result"?> -->
         <!-- 100 -->
-        <td><input type="text" name="v1" value=<?php echo"$result"?> ></td>
+        <td><input type="text" name="v100" value=<?php echo"$v100"?>        ></td>
          <!-- 7 -->
-        <td><input type="text" name="v2" ></td>
+        <td><input type="text" name="v7" value=<?php echo"$v7"?> ></td>
          <!-- 107 -->
-        <td><input type="text" name="v3" value=<?php echo"3"?>></td>
+        <td><input type="text" name="v107"value=<?php echo"$v107"?> ></td>
          <!-- 3 -->
-        <td><input type="text" name="v4" value=<?php echo"4"?>></td>
+        <td><input type="text" name="v3" value=<?php echo"$v3"?> ></td>
          <!-- 104 -->
-        <td><input type="text" name="v5" value=<?php echo"5"?>></td>
+        <td><input type="text" name="v104" value=<?php echo"$v104"?> ></td>
     </tr>
     </tbody>
     </table>
