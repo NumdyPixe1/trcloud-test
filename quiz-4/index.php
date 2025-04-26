b5fa28390a0eb51a9ca29eb9a3ea716ab30288ca
NumdyPixe1 <numdao123@gmail.com>
2025-04-26 20:29:49 +0700

    clean code

--- quiz-4/index.php
+++ quiz-4/index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>quiz-4</title>
-    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="index.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <table style="width: 100%;">
@@ -25,7 +26,6 @@
     </tbody>
     </table>
     
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
        const ctx1 = document.getElementById('barChart');
        const ctx2 = document.getElementById('pieChart');
@@ -36,90 +36,89 @@ $(document).ready(function (){
         method: "get",
         success: function (res){
             // console.log(res); //raw data
-            let data = (JSON.parse(res));
-        let cityData = $.map( data, function( index ) {
+            const data = (JSON.parse(res));
+            const cityData = $.map( data, function( index ){
             return  [index.City];
             });
-        let populationData = $.map( data, function( index ) {
+            const populationData = $.map( data, function( index ){
                 return  [index.Population];
                 });
-        const countryData = $.map( data, function( index ) {
+            const countryData = $.map( data, function( index ){
                     return  [index.Country];
                     });
-               console.log(populationData); 
 
-               new Chart(ctx1, {
-    type: 'bar',
-    data: {
-      labels: cityData,
-      datasets: [{
-        label: labelChart,
-        data: populationData,
-        borderWidth: 1
-        ,backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-      ],    borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-      ],
-  
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-  new Chart(ctx2, {
-    type: 'pie',
-    data: {
-      labels: cityData,
-      datasets: [{
-        label: labelChart,
-        data: populationData,
-        borderWidth: 1
-        ,backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-      ],    borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-      ],
-  
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+                new Chart(ctx1, {
+                  type: 'bar',
+                  data: {
+                    labels: cityData,
+                    datasets: [{
+                      label: labelChart,
+                      data: populationData,
+                      borderWidth: 1
+                      ,backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                      'rgba(255, 205, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(201, 203, 207, 0.2)'
+                    ],    borderColor: [
+                      'rgb(255, 99, 132)',
+                      'rgb(255, 159, 64)',
+                      'rgb(255, 205, 86)',
+                      'rgb(75, 192, 192)',
+                      'rgb(54, 162, 235)',
+                      'rgb(153, 102, 255)',
+                      'rgb(201, 203, 207)'
+                    ],
+                
+                    }]
+                  },
+                  options: {
+                    scales: {
+                      y: {
+                        beginAtZero: true
+                      }
+                    }
+                  }
+                });
+                new Chart(ctx2, {
+                  type: 'pie',
+                  data: {
+                    labels: cityData,
+                    datasets: [{
+                      label: labelChart,
+                      data: populationData,
+                      borderWidth: 1
+                      ,backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                      'rgba(255, 205, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(201, 203, 207, 0.2)'
+                    ],    borderColor: [
+                      'rgb(255, 99, 132)',
+                      'rgb(255, 159, 64)',
+                      'rgb(255, 205, 86)',
+                      'rgb(75, 192, 192)',
+                      'rgb(54, 162, 235)',
+                      'rgb(153, 102, 255)',
+                      'rgb(201, 203, 207)'
+                    ],
+                
+                    }]
+                  },
+                  options: {
+                    scales: {
+                      y: {
+                        beginAtZero: true
+                      }
+                    }
+                  }
+                });
         },
         error: function (err){
             console.log(err);
